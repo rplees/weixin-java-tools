bf1ba869d4ef29c379d1ef1f6af64ca7fe230884
rplees <rplees.i.ly@gmail.com>
2015-10-14 14:26:13 +0800

    no message

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/util/RandomUtils.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/util/RandomUtils.java
@@ -2,16 +2,30 @@ package me.chanjar.weixin.common.util;
 
 public class RandomUtils {
 
-  private static final String RANDOM_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	private static final String RANDOM_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  private static final java.util.Random RANDOM = new java.util.Random();
+	private static final java.util.Random RANDOM = new java.util.Random();
 
-  public static String getRandomStr() {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 16; i++) {
-      sb.append(RANDOM_STR.charAt(RANDOM.nextInt(RANDOM_STR.length())));
-    }
-    return sb.toString();
-  }
+	public static String getRandomStr() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 16; i++) {
+			sb.append(RANDOM_STR.charAt(RANDOM.nextInt(RANDOM_STR.length())));
+		}
+		return sb.toString();
+	}
 
+	public static void main(String[] args) {
+		java.util.Random RANDOM = new java.util.Random(1);
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(RANDOM.nextDouble());
+		}
+
+		System.out.println("================");
+		RANDOM = new java.util.Random(1);
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(RANDOM.nextDouble());
+		}
+	}
 }
